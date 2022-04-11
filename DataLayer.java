62c2e1ae7cda6767fba1961de3c63e902da54a90
vs7050 <70558928+vs7050@users.noreply.github.com>
2022-04-11 18:51:13 -0400

    base datalayer and presentation files

--- DataLayer.java
+++ DataLayer.java
@@ -1,9 +1,123 @@
-import java.sql.*;
+// Vincent Sze
+// Practical 01 - ISTE-330
+// 2/18/22
 
-public class DataLayer {
-  
-  public static void main(String[] args) {
-      String text = "";
-  }
-  
+import java.sql.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
+import javax.swing.*;
+
+public class TravelDatabase {
+   private Connection conn;
+   private Statement stmt;
+   private ResultSet rs;
+   private String sql;
+   
+   final String DEFAULT_DRIVER = "com.mysql.cj.jdbc.Driver";
+   final String url = "jdbc:mysql://localhost/travel";
+   
+   public TravelDatabase() {
+   }
+   
+   public boolean connect() {
+      String userName = "";
+      Scanner s = new Scanner(System.in);
+      String password = new String();
+      String passwordInput = new String();
+      
+      try {
+         Class.forName(DEFAULT_DRIVER);
+         
+         System.out.print("Username: ");
+         userName = GetInput.readLine();
+         
+         System.out.print("Password: ");
+         passwordInput = s.nextLine();
+         
+         if (passwordInput.equals("")) {
+            password = "student";
+         }
+         else {
+            password = passwordInput;
+         }
+         
+         conn = DriverManager.getConnection(url, userName, password);
+      } catch (ClassNotFoundException cnfe) {
+         System.out.println("Database connection failed!");
+         System.out.println("CLASS ERROR -> " + cnfe);
+         System.exit(0);
+      } catch (SQLException sqle) {
+         System.out.println("Database connection failed!");
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         System.exit(0);
+      }
+      return conn != null;
+   }
+   
+   public int countStaff() {
+      int row = 0;
+      try {
+         stmt = conn.createStatement();
+         sql = "SELECT COUNT(*) FROM staff";
+         rs = stmt.executeQuery(sql);
+         rs.next();
+         row = rs.getInt(1);
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         System.exit(0);
+      }
+      return row;
+   }
+   
+   public int deletePhoneRecords(int passengerID) {
+      int numRowsDel = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("DELETE FROM phone WHERE passengerID = ?");
+         stmt.setInt(1, passengerID);
+         
+         numRowsDel = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         System.exit(0);
+      }
+      return numRowsDel;
+   }
+   
+   public int updateEquipment(int equipID, String equipmentName, String equipmentDescription, int equipmentCapacity) {
+      int numRowsUpdated = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("UPDATE equipment SET equipmentName = ?, equipmentDescription = ?, equipmentCapacity = ? WHERE equipID = ?");
+         stmt.setString(1, equipmentName);
+         stmt.setString(2, equipmentDescription);
+         stmt.setInt(3, equipmentCapacity);
+         stmt.setInt(4, equipID);
+         
+         numRowsUpdated = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         System.exit(0);
+      }
+      return numRowsUpdated;
+   }
+   
+   public boolean close() {
+      try {
+         if (conn != null) {
+            conn.close();
+         }
+         if (stmt != null) {
+            stmt.close();
+         }
+         return true;
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return false;
+      }
+   }
+   
+   public static void main(String[] args) {
+      System.out.println("Author: Sze, Vincent");
+      new TravelDatabase();
+   }
 }
--- Presentation.java
+++ Presentation.java
@@ -1,9 +1,51 @@
-import java.util.*;
+// Vincent Sze
+// Practical 01 - ISTE-330
+// 2/18/22
+
+import java.sql.*;
+import javax.swing.JOptionPane;
 
 public class Presentation {
-  
-  public static void main(String[] args) {
-    String text = "";
-  }
-  
+   TravelDatabase dl = new TravelDatabase();
+   private int columns;
+   
+   public Presentation() {
+      System.out.println("Connecting to travel database...");
+      dl.connect();
+      System.out.println("DB Connected(Sze, Vincent)");
+      
+      columns = dl.countStaff();
+      System.out.println("Number of records in staff: " + columns);
+      
+      int passengerID = 0;
+      int equipID = 0;
+      String equipmentName = "";
+      String equipmentDescription = "";
+      int equipmentCapacity = 0;
+
+      System.out.print("Which person's phone would you like to delete: ");
+      passengerID = GetInput.readLineInt();
+      dl.deletePhoneRecords(passengerID);
+      
+      System.out.print("Which equipment ID would you like to update?");
+      equipID = GetInput.readLineInt();
+      
+      System.out.print("Updated equipment name: ");
+      equipmentName = GetInput.readLine();
+      
+      System.out.print("Updated equipment description: ");
+      equipmentDescription = GetInput.readLine();
+      
+      System.out.print("Updated equipment capacity: ");
+      equipmentCapacity = GetInput.readLineInt();
+      dl.updateEquipment(equipID, equipmentName, equipmentDescription, equipmentCapacity);
+      
+      dl.close();
+   }
+
+   public static void main(String[] args) {
+      System.out.println("Author: Sze, Vincent");
+      JOptionPane.showMessageDialog(null, "Sze, Vincent", "Author", JOptionPane.INFORMATION_MESSAGE);
+      new Presentation();
+   }
 }
