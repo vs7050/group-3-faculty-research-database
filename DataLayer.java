db11ccc39dd3f94a42bae1c2757246d17fa43656
vs7050 <70558928+vs7050@users.noreply.github.com>
2022-04-15 04:12:13 -0400

    made code tidy, added small comments to each function, added skeleton functions for the rest of the non-finished functions

--- DataLayer.java
+++ DataLayer.java
@@ -49,8 +49,109 @@ public class DataLayer {
          System.exit(0);
       }
       return conn != null;
-   }
+   } // connect
+
+   /* INTEREST */
+
+   public int createInterest(String interest) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Interest records added -> " + records);
+      return records;
+   } // createInterest
+
+   public int updateInterest(int interestID, String interest) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Interest records updated -> " + records);
+      return records;
+   } // updateInterest
+
+   public int deleteInterest(int interestID, String interest) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Interest records deleted -> " + records);
+      return records;
+   } // deleteInterest
+
+   /* ABSTRACT */
+
+   public int createAbstract(String abs, String absDate) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Abstract records added -> " + records);
+      return records;
+   } // createAbstract
+
+   public int udpateAbstract(int absID, String abs, String absDate) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Abstract records updated -> " + records);
+      return records;
+   } // udpateAbstract
+
+   public int deleteAbstract(String abs, String absDate) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Abstract records deleted -> " + records);
+      return records;
+   } // deleteAbstract
    
+   /* STUDENT */
+
    public int createStudent(String fname, String lname) {
       int records = 0;
       try {
@@ -61,172 +162,283 @@ public class DataLayer {
       } catch (SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
          return 0;
-         
       }
-      System.out.println("Number of records added to Student -> " + records);
+
+      System.out.println("Number of Student records added -> " + records);
       return records;
-   }
+   } // createStudent
    
-   public int updateStudent(int studentID, String fname, String lname){
-   
+   public int updateStudent(int studentID, String fname, String lname) {
       int records = 0;
-      try{
-      
+      try {
          PreparedStatement stmt = conn.prepareStatement("UPDATE student SET fname = ?, lname = ? WHERE studentID = ?");
          stmt.setString(1, fname);
          stmt.setString(2, lname);
          stmt.setInt(3, studentID);
          records = stmt.executeUpdate();
-      
-      }
-      catch(SQLException sqle){
-      
+      } catch(SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
          return 0;
-      
       }
-      
+
       System.out.println("Number of Student records updated -> " + records);
       return records;
-   
-   }
+   } // updateStudent
    
    public int deleteStudent(int studentID){
-   
       int records = 0;
-      try{
-      
+      try {
          PreparedStatement stmt = conn.prepareStatement("DELETE FROM student WHERE studentID = ?");
          stmt.setInt(1, studentID);
          records = stmt.executeUpdate();
-      
-      }
-      catch(SQLException sqle){
-      
+      } catch(SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " +sqle);
          return 0;
-      
       }
+
       System.out.println("Number of Student records deleted -> " + records);
       return records;
-      
-   
-   }
-   
-   public int createFaculty(String lName, String fName)
-   {
+   } // deleteStudent
+
+   public int createStudentContact(int studentID, int roomNumber, String phoneNumber, String emailAddress) {
       int records = 0;
-      try
-      {
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         records = stmt.executeUpdate();
+      } catch(SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " +sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Student Contacts updated -> " + records);
+      return records;
+   } // createStudentContact
+
+   public int updateStudentContact(int studentID, int roomNumber, String phoneNumber, String emailAddress) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         stmt.setInt(3, );
+         records = stmt.executeUpdate();
+      } catch(SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Student Contacts updated -> " + records);
+      return records;
+   } // updateStudentContact
+
+   public int deleteStudentContact(int studentID, int roomNumber, String phoneNumber, String emailAddress) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         stmt.setInt(3, );
+         records = stmt.executeUpdate();
+      } catch(SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Student Contacts deleted -> " + records);
+      return records;
+   } // deleteStudentContact
+
+   public int createStudentInterest(int studentID, int interestID) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         records = stmt.executeUpdate();
+      } catch(SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " +sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Student Interests added -> " + records);
+      return records;
+   } // createStudentInterest
+
+   public int updateStudentInterest(int studentID, int interestID) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         stmt.setInt(3, );
+         records = stmt.executeUpdate();
+      } catch(SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Student Interests updated -> " + records);
+      return records;
+   } // updateStudentInterest
+
+   public int deleteStudentInterest(int studentID, int interestID) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setString(1, );
+         stmt.setString(2, );
+         stmt.setInt(3, );
+         records = stmt.executeUpdate();
+      } catch(SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+
+      System.out.println("Number of Student Interests deleted -> " + records);
+      return records;
+   } // deleteStudentInterest
+   
+   /* FACULTY */
+
+   public int createFaculty(String lName, String fName) {
+      int records = 0;
+      try {
          PreparedStatement stmt = conn.prepareStatement("INSERT INTO Faculty (lName, fName) VALUES (?, ?)");
          stmt.setString(1, lName);
          stmt.setString(2, fName);
          records = stmt.executeUpdate();
-      }
-      catch (SQLException sqle) 
-      {
+      } catch (SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
       }
-      System.out.println("Number of records added to Faculty -> " + records);
+
+      System.out.println("Number of Faculty records added -> " + records);
       return records;
-   }
+   } // createFaculty
    
-   public int updateFaculty(int facultyID, String lName, String fName)
-   {
+   public int updateFaculty(int facultyID, String lName, String fName) {
       int records = 0;
-      try
-      {
+      try {
          PreparedStatement stmt = conn.prepareStatement("UPDATE Faculty SET lName = ?, fName = ? WHERE facultyID = ?");
          stmt.setString(1, lName);
          stmt.setString(2, fName);
          stmt.setInt(3, facultyID);
          records = stmt.executeUpdate();
-      }
-      catch (SQLException sqle) 
-      {
+      } catch (SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
       }
+
       System.out.println("Number of Faculty records updated -> " + records);
       return records;
-   }
+   } // updateFaculty
    
-   public int deleteFaculty(int facultyID)
-   {
+   public int deleteFaculty(int facultyID) {
       int records = 0;
-      try
-      {
+      try {
          PreparedStatement stmt = conn.prepareStatement("DELETE FROM Faculty WHERE facultyID = ?");
          stmt.setInt(1, facultyID);
          records = stmt.executeUpdate();
-      }
-      catch (SQLException sqle) 
-      {
+      } catch (SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
          return 0;
       }
       System.out.println("Number of Faculty records deleted -> " + records);
       return records;
-   }
-   
-   public int createFacultyInterest(int facultyID, int interestID)
-   {
+   } // deleteFaculty
+
+   public int createFacultyContact(int facultyID, int roomNumber, String phoneNumber, String emailAddress) {
       int records = 0;
-      try
-      {
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setInt(1, );
+         stmt.setInt(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+      System.out.println("Number of Faculty Contacts added -> " + records);
+      return records;
+   } // createFacultyInterest
+
+   public int updateFacultyContact(int facultyID, int roomNumber, String phoneNumber, String emailAddress) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setInt(1, );
+         stmt.setInt(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+      System.out.println("Number of Faculty Contacts updated -> " + records);
+      return records;
+   } // updateFacultyContact
+
+   public int deleteFacultyContact(int facultyID, int roomNumber, String phoneNumber, String emailAddress) {
+      int records = 0;
+      try {
+         PreparedStatement stmt = conn.prepareStatement("");
+         stmt.setInt(1, );
+         stmt.setInt(2, );
+         records = stmt.executeUpdate();
+      } catch (SQLException sqle) {
+         System.out.println("ERROR MESSAGE -> " + sqle);
+         return 0;
+      }
+      System.out.println("Number of Faculty Contacts deleted -> " + records);
+      return records;
+   } // deleteFacultyContact
+   
+   public int createFacultyInterest(int facultyID, int interestID) {
+      int records = 0;
+      try {
          PreparedStatement stmt = conn.prepareStatement("INSERT INTO FacultyInterest (facultyID, interestID) VALUES (?, ?)");
          stmt.setInt(1, facultyID);
          stmt.setInt(2, interestID);
          records = stmt.executeUpdate();
-      }
-      catch (SQLException sqle) 
-      {
+      } catch (SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
          return 0;
       }
-      System.out.println("Number of records added to FacultyInterest -> " + records);
+      System.out.println("Number of Faculty Interests added -> " + records);
       return records;
-   }
+   } // createFacultyInterest
    
-   public int updateFacultyInterest(int facultyID, int interestID)
-   {
+   public int updateFacultyInterest(int facultyID, int interestID) {
       int records = 0;
-      try
-      {
+      try {
          PreparedStatement stmt = conn.prepareStatement("UPDATE FacultyInterest SET interestID = ? WHERE facultyID = ?");
          stmt.setInt(1, interestID);
          stmt.setInt(2, facultyID);
          records = stmt.executeUpdate();
-      }
-      catch (SQLException sqle) 
-      {
+      } catch (SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
          return 0;
       }
-      System.out.println("Number of FacultyInterest records updated -> " + records);
+      System.out.println("Number of Faculty Interests updated -> " + records);
       return records;
-   }
+   } // updateFacultyInterest
    
-   public int deleteFacultyInterest(int facultyID, int interestID)
-   {
+   public int deleteFacultyInterest(int facultyID, int interestID) {
       int records = 0;
-      try
-      {
+      try {
          PreparedStatement stmt = conn.prepareStatement("DELETE FROM Faculty WHERE facultyID = ? AND interestID = ?");
          stmt.setInt(1, facultyID);
          stmt.setInt(2, interestID);
          records = stmt.executeUpdate();
-      }
-      catch (SQLException sqle) 
-      {
+      } catch (SQLException sqle) {
          System.out.println("ERROR MESSAGE -> " + sqle);
          return 0;
       }
-      System.out.println("Number of FacultyInterest records deleted -> " + records);
+      System.out.println("Number of Faculty Interests deleted -> " + records);
       return records;
-   }
+   } // deleteFacultyInterest
       
-
-   
    public boolean close() {
       try {
          if (conn != null) {
@@ -240,10 +452,10 @@ public class DataLayer {
          System.out.println("ERROR MESSAGE -> " + sqle);
          return false;
       }
-   }
+   } // close
    
    public static void main(String[] args) {
-      System.out.println("Author: Sze, Vincent");
+      System.out.println("Author: Group 3");
       new DataLayer();
-   }
-}
+   } // main method
+} // end of DataLayer
