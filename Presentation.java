139b0236f78ccd5c6df34ee7864b6051cecd99f3
jdl1209 <54865107+jdl1209@users.noreply.github.com>
2022-04-15 18:54:19 -0400

    Added some faculty functionality to the Presentation

--- Presentation.java
+++ Presentation.java
@@ -1,15 +1,24 @@
-
-
 import java.sql.*;
 import javax.swing.JOptionPane;
+import java.util.*;
 
 public class Presentation {
+
+   String facultyUser = "faculty";
+   String facultyPassword = "faculty123";
    DataLayer dl = new DataLayer();
-   
+   Scanner scn = new Scanner(System.in);
    
    public Presentation() {
-      System.out.println("Connecting to faculty database...");
+      System.out.println("Attempting to connect to the database...\n");
       dl.connect();
+      System.out.print("\nWould you like to log in as faculty? (Y/N): ");
+      String yesOrNo = scn.nextLine();
+      if(yesOrNo.equals("Y")){
+      
+         doFacultyLoop();
+      
+      }
       
       dl.close();
    }
@@ -18,5 +27,46 @@ public class Presentation {
       System.out.println("Author: Group 3");
       JOptionPane.showMessageDialog(null, "Group 3", "Author", JOptionPane.INFORMATION_MESSAGE);
       new Presentation();
+   }
+   
+   public void doFacultyLoop(){
+   
+      System.out.print("\nEnter a faculty username: ");
+      String user = scn.nextLine();
+      System.out.print("\nEnter faculty password: ");
+      String pass = scn.nextLine();
+      if(user.equals(facultyUser) && pass.equals(facultyPassword)){
+      
+         System.out.println("Faculty credentials accepted.. \nLogging in!");
+         
+      
+      }
+      else{
+      
+         return;
+      
+      }
+      int choice = 0;
+      while(choice != 3){
+      
+         System.out.println("-----------------------------------------------------------------------");
+         System.out.print("\nEnter an option:\n1. Add a faculty\n2. Add an abstract\n3. Exit\n\nSelection: ");
+         choice = Integer.parseInt(scn.nextLine());
+         switch(choice){
+            case 1:
+               System.out.print("\nEnter a faculty lastname: ");
+               String lName = GetInput.readLine();
+               System.out.print("\nEnter a faculty firstname: ");
+               String fName = GetInput.readLine();
+               dl.createFaculty(lName, fName);
+               break;
+         
+         
+         }
+      
+      }
+      
+      
+   
    }
 }
